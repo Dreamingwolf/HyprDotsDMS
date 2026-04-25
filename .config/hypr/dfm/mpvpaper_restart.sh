c23aa8c99254154f3bb00bf82a6502ca2a3605ea
David Muller <dreamingwolfocf@gmail.com>
2026-04-25 16:05:29 -0500

    add configs for kitty, fastfetch, and obsidian

--- .config/hypr/dfm/mpvpaper_restart.sh
+++ .config/hypr/dfm/mpvpaper_restart.sh
@@ -6,5 +6,5 @@ pkill '^mpvpape'
 sleep 0.2
 
 # Start a fresh background instance detached from the terminal.
-mpvpaper -o "no-audio --auto-stop --profile=high-quality --loop-file=inf" ALL /home/david/Pictures/Wallpapers/Live &
+mpvpaper -o "no-audio --auto-stop --profile=high-quality shuffle --loop-file=inf" ALL /home/david/Pictures/Wallpapers/Live &
 return 2> /dev/null; exit
--- .zshrc
+++ .zshrc
@@ -115,6 +115,7 @@ source $ZSH/oh-my-zsh.sh
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 fastfetch
 
